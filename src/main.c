010095b2bbb1067b3911b1009e11e91095f12264
Andrew Elsayeh <andrewelsayeh@gmail.com>
2023-03-14 09:47:33 +0100

    Not much done

--- src/main.c
+++ src/main.c
@@ -4,17 +4,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <inttypes.h>
-#include <stddef.h>
-#include <stdint.h>
+// #include <inttypes.h>
+// #include <stddef.h>
+// #include <stdint.h>
 
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/adc.h>
+// #include <zephyr/device.h>
+// #include <zephyr/devicetree.h>
+// #include <zephyr/drivers/adc.h>
+// #include <zephyr/sys/printk.h>
+// #include <zephyr/sys/util.h>
+
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/sys/util.h>
-
 #include "adc_reader/adc_reader.h"
 
 
@@ -28,6 +28,6 @@ void main(void)
 
 	while (1) {	
 		readAndPrintAllChannels();
-		k_sleep(K_MSEC(200));
+		k_sleep(K_MSEC(100));
 	}
 }
