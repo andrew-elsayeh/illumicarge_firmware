88d400e2a836911bc329063feb63aa852b0e26a3
Andrew Elsayeh <andrewelsayeh@gmail.com>
2023-03-14 11:32:37 +0100

    Merge code from samples/basic/button into user_interface

--- src/user_interface/user_interface.c
+++ src/user_interface/user_interface.c
@@ -1,7 +1,77 @@
 #include "user_interface.h"
 
 
-uint32_t led_brightness_percentage = -5;
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/printk.h>
+#include <inttypes.h>
+
+
+
+uint32_t led_brightness_percentage = 0;
+
+
+
+#define SLEEP_TIME_MS	1
+
+/*
+ * Get button configuration from the devicetree sw0 alias. This is mandatory.
+ */
+#define SW0_NODE	DT_ALIAS(sw0)
+#if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
+#error "Unsupported board: sw0 devicetree alias is not defined"
+#endif
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
+							      {0});
+static struct gpio_callback button_cb_data;
+
+/*
+ * The led0 devicetree alias is optional. If present, we'll use it
+ * to turn on the LED whenever the button is pressed.
+ */
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
+						     {0});
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb,
+		    uint32_t pins)
+{
+    led_brightness_percentage = led_brightness_percentage + 10;
+	// printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+
+}
+
+
+void _init_user_interface(void)
+{
+    int ret;
+
+	if (!gpio_is_ready_dt(&button)) {
+		printk("Error: button device %s is not ready\n",
+		       button.port->name);
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure %s pin %d\n",
+		       ret, button.port->name, button.pin);
+		return;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&button,
+					      GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret != 0) {
+		printk("Error %d: failed to configure interrupt on %s pin %d\n",
+			ret, button.port->name, button.pin);
+		return;
+	}
+
+	gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+	gpio_add_callback(button.port, &button_cb_data);
+}
+
 
 
 int32_t getLEDBrightnessPercentage(UserInterface_t *self)
@@ -13,6 +83,8 @@ int32_t getLEDBrightnessPercentage(UserInterface_t *self)
 uint8_t initUserInterface(UserInterface_t *UserInterface)
 {
     UserInterface->getLEDBrightnessPercentage = getLEDBrightnessPercentage;
+
+    _init_user_interface();
 
     return 0;
 
