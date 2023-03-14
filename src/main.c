347b1798b0c5433a8b557e65da5842183ee1cf09
Andrew Elsayeh <andrewelsayeh@gmail.com>
2023-03-14 10:52:58 +0100

    Create test setup for user interface

--- src/main.c
+++ src/main.c
@@ -15,31 +15,33 @@
 // #include <zephyr/sys/util.h>
 
 #include <zephyr/kernel.h>
+
 #include "adc_reader/adc_reader.h"
 
-
-
-
+#include "user_interface/user_interface.h"
 
 void main(void)
 {
 
-
-
-	/* For Testing ADC Reader*/
-	ADCReader_t ADCReader;
-	initADCReader(&ADCReader);
-	while (1) {	
-		printk("ADC1_mV: %dmV\n", ADCReader.getADC1_mV(&ADCReader));
-		printk("ADC2_mV: %dmV\n", ADCReader.getADC2_mV(&ADCReader));
-		printk("V_BAT_ADC_mV: %dmV\n", ADCReader.getV_BAT_TEMP_ADC_mV(&ADCReader));
-		printk("V_BAT_TEMP_ADC_mV: %dmV\n", ADCReader.getV_BAT_ADC_mV(&ADCReader));
-		printk("BAT_CUR_mV: %dmV\n", ADCReader.getBAT_CUR_mV(&ADCReader));
-		k_sleep(K_MSEC(100));
-	}
+	// /* For Testing ADC Reader*/
+	// ADCReader_t ADCReader;
+	// initADCReader(&ADCReader);
+	// while (1) {	
+	// 	printk("ADC1_mV: %dmV\n", ADCReader.getADC1_mV(&ADCReader));
+	// 	printk("ADC2_mV: %dmV\n", ADCReader.getADC2_mV(&ADCReader));
+	// 	printk("V_BAT_ADC_mV: %dmV\n", ADCReader.getV_BAT_TEMP_ADC_mV(&ADCReader));
+	// 	printk("V_BAT_TEMP_ADC_mV: %dmV\n", ADCReader.getV_BAT_ADC_mV(&ADCReader));
+	// 	printk("BAT_CUR_mV: %dmV\n", ADCReader.getBAT_CUR_mV(&ADCReader));
+	// 	k_sleep(K_MSEC(100));
+	// }
 
 	/* For Teting User Input*/
-	// while(1){
+	// UserInterface_t UserInterface;
+	// initUserInterface(&UserInterface);
 
-	// }
+	while(1){
+
+		printk("LED Brighness: %d \n", -1);
+		k_sleep(K_MSEC(100));
+	}
 }
