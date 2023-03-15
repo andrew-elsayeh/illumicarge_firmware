56c2726ca53b251e0ee72b229e3dda283c0a788d
Andrew Elsayeh <andrewelsayeh@gmail.com>
2023-03-15 12:36:55 +0100

    Reorganized tests

---
+++ src/tests.h
@@ -0,0 +1,19 @@
+#include <zephyr/kernel.h>
+
+#include "adc_reader/adc_reader.h"
+
+#include "user_interface/user_interface.h"
+
+#include "pwm_controller/pwm_controller.h"
+
+#include "power_path_controller/power_path_controller.h"
+
+void test_end_to_end_manual(void);
+
+void test_adc(void);
+
+void test_ui(void);
+
+void test_pwm_controller(void);
+
+void test_power_path_controller(void);
