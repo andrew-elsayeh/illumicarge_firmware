7f62af3c353a0c42aef1074b157e6c6706521b9f
Andrew Elsayeh <andrewelsayeh@gmail.com>
2023-03-15 11:15:37 +0100

    Created end to end test

--- src/main.c
+++ src/main.c
@@ -22,6 +22,14 @@
 
 #include "pwm_controller/pwm_controller.h"
 
+void clear_console(void)
+{
+	printk("\n");
+	printk("\033[2J");
+	printk("\033[2J");
+	printk("\n");
+}
+
 void main(void)
 {
 
@@ -53,26 +61,60 @@ void main(void)
 	// }
 
 
-	/*PWM Controller Unit Test*/
+	// /*PWM Controller Unit Test*/
+	// PWMController_t PWMController;
+	// initPWMController(&PWMController);
+
+	// printk("LED should fade on and off and brightness displayed on terminal.. \n");
+
+	// while(1){
+		
+	// 	for(int i = 0;i < 100; i++)
+	// 	{
+	// 		PWMController.setLEDBrightness(i);
+	// 		printk("Brightness: %d \n", PWMController.getLEDBrightnessPercentage());
+	// 		k_sleep(K_MSEC(10));
+	// 	}
+	// 	for(int i = 0;i < 100; i++)
+	// 	{
+	// 		PWMController.setLEDBrightness(100-i);
+	// 		printk("Brightness: %d \n", PWMController.getLEDBrightnessPercentage());
+	// 		k_sleep(K_MSEC(10));
+	// 	}
+	// }
+
+	/**
+	 * End-to-End Test 
+	 */
+
+	ADCReader_t ADCReader;
+	initADCReader(&ADCReader);
+
+	UserInterface_t UserInterface;
+	initUserInterface(&UserInterface);
+
 	PWMController_t PWMController;
 	initPWMController(&PWMController);
 
-	printk("LED should fade on and off and brightness displayed on terminal.. \n");
-
+	printk("End-to-End (Unit) Test:  Displays ADC readings of all channels, User Input controls the status LEDs as well as the PWM Brightness\n");
 	while(1){
+		clear_console();
+		//ADC
+		printk("ADC1: %5dmV     ", ADCReader.getADC1_mV(&ADCReader));
+		printk("ADC2: %5dmV     ", ADCReader.getADC2_mV(&ADCReader));
+		printk("V_BAT_ADC: %5dmV     ", ADCReader.getV_BAT_TEMP_ADC_mV(&ADCReader));
+		printk("V_BAT_TEMP_ADC: %5dmV     ", ADCReader.getV_BAT_ADC_mV(&ADCReader));
+		printk("BAT_CUR: %5dmV\n", ADCReader.getBAT_CUR_mV(&ADCReader));
 		
-		for(int i = 0;i < 100; i++)
-		{
-			PWMController.setLEDBrightness(i);
-			printk("Brightness: %d \n", PWMController.getLEDBrightnessPercentage());
-			k_sleep(K_MSEC(10));
-		}
-		for(int i = 0;i < 100; i++)
-		{
-			PWMController.setLEDBrightness(100-i);
-			printk("Brightness: %d \n", PWMController.getLEDBrightnessPercentage());
-			k_sleep(K_MSEC(10));
-		}
-	}
+		//UI
+		uint32_t user_input = UserInterface.getUserInputLEDBrightnessPercent();
+		printk("User Input LED Brighness Percentage: %d \n", user_input);
+		UserInterface.setStatusLEDs(user_input);
+		
+		//PWM
+		PWMController.setLEDBrightness(user_input);
+		printk("PWM Brightness: %d \n", PWMController.getLEDBrightnessPercentage());
 
+		k_sleep(K_MSEC(200));
+	}
 }
